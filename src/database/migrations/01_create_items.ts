99d583a744fd9657bdc746440eeee31907ec9a41
mendesmcmg <mariagoncalves@cjr.org.br>
2020-06-06 22:41:23 -0300

    Points and items controllers

--- src/database/migrations/01_create_items.ts
+++ src/database/migrations/01_create_items.ts
@@ -4,11 +4,10 @@ export async function up(knex: Knex) {
     return knex.schema.createTable('items', table => {
         table.increments('id').primary();
         table.string('image').notNullable();
-        table.string('name').notNullable();
         table.string('title').notNullable();
     });
 }
 
 export async function down(knex: Knex) {
-    return knex.schema.dropTable('item');
+    return knex.schema.dropTable('items');
 }
--- src/database/seeds/create_items.ts
+++ src/database/seeds/create_items.ts
@@ -1,7 +1,7 @@
-import Knex from "knex";
+import Knex from 'knex';
 
 export async function seed(knex: Knex) {
-    knex('items').insert([
+    await knex('items').insert([
         { title: 'Lâmpadas', image: 'lampadas.svg' },
         { title: 'Pilhas e Baterias', image: 'baterias.svg' },
         { title: 'Papeis e Papelão', image: 'papeis-papelao.svg' },
